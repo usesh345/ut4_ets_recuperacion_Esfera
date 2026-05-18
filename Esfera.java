5a8c74644c11319e09b1a57b1d945404be0a15f4
Andrés Felipe Jaimes Martínez <andresfelipeja51@gmail.com>
2026-05-18 10:22:11 +0100

    comentarios  Javadocs

--- Esfera.java
+++ Esfera.java
@@ -1,37 +1,80 @@
+/**
+ * The type Esfera.
+ */
 public class Esfera {
     private double radio;
     private double area;
     private double volume;
 
+    /**
+     * Instantiates a new Esfera.
+     */
     public Esfera() {
     }
 
+    /**
+     * Instantiates a new Esfera.
+     *
+     * @param radio  the radio
+     * @param area   the area
+     * @param volume the volume
+     */
     public Esfera(double radio, double area, double volume) {
         this.radio = radio;
         this.area = area;
         this.volume = volume;
     }
 
+    /**
+     * Gets radio.
+     *
+     * @return the radio
+     */
     public double getRadio() {
         return radio;
     }
 
+    /**
+     * Sets radio.
+     *
+     * @param radio the radio
+     */
     public void setRadio(double radio) {
         this.radio = radio;
     }
 
+    /**
+     * Gets area.
+     *
+     * @return the area
+     */
     public double getArea() {
         return area;
     }
 
+    /**
+     * Sets area.
+     *
+     * @param area the area
+     */
     public void setArea(double area) {
         this.area = area;
     }
 
+    /**
+     * Gets volume.
+     *
+     * @return the volume
+     */
     public double getVolume() {
         return volume;
     }
 
+    /**
+     * Sets volume.
+     *
+     * @param volume the volume
+     */
     public void setVolume(double volume) {
         this.volume = volume;
     }
--- Main.java
+++ Main.java
@@ -1,20 +1,43 @@
 import javax.swing.*;
 
+/**
+ * The type Main.
+ */
 public class Main {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
+
         double radio;
 
         radio = Double.parseDouble(JOptionPane.showInputDialog(null,"ingrese el valor del radio:"));
         double area= calcularArea(radio);
-        double volimen= calcularVolumen(radio);
-        JOptionPane.showMessageDialog(null, "El volumen del cilindro es: " + volimen + " El área del cilindro es: " + area);
+        double volumen = calcularVolumen(radio);
+        JOptionPane.showMessageDialog(null, "El volumen del cilindro es: " + volumen + " El área del cilindro es: " + area);
 
     }
 
+    /**
+     * Calcular area double.
+     *
+     * @param radio the radio
+     * @return the double
+     */
     public static double calcularArea(double radio) {
         return 4 * Math.PI * radio * radio;
     }
+
+    /**
+     * Calcular volumen double.
+     *
+     * @param radio the radio
+     * @return the double
+     */
     public static double calcularVolumen(double radio) {
         return (4.0 / 3.0) * Math.PI * radio * radio * radio;
     }
+
 }
