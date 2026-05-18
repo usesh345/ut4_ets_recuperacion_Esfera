d423967614cbaa2733c2d5a276dbc008a2ddf421
Andrés Felipe Jaimes Martínez <andresfelipeja51@gmail.com>
2026-05-18 09:59:38 +0100

    creacion de metodos calcularArea calcularVolumen

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+
+    }
+
+    public double calcularArea(double radio) {
+        return 4 * Math.PI * radio * radio;
+    }
+    public double calcularVolumen(double radio) {
+        return (4.0 / 3.0) * Math.PI * radio * radio * radio;
     }
 }
