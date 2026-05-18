d88bda9bd32d69ab88804fd116a811c99462c971
Andrés Felipe Jaimes Martínez <andresfelipeja51@gmail.com>
2026-05-18 10:07:21 +0100

    Solicitar el valor del radio, e imprimir
    imprime el volumen y el area del cilindro

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        double radio;
 
+        radio = Double.parseDouble(JOptionPane.showInputDialog(null,"ingrese el valor del radio:"));
+        double area= calcularArea(radio);
+        double volimen= calcularVolumen(radio);
+        JOptionPane.showMessageDialog(null, "El volumen del cilindro es: " + volimen + " El área del cilindro es: " + area);
 
     }
 
-    public double calcularArea(double radio) {
+    public static double calcularArea(double radio) {
         return 4 * Math.PI * radio * radio;
     }
-    public double calcularVolumen(double radio) {
+    public static double calcularVolumen(double radio) {
         return (4.0 / 3.0) * Math.PI * radio * radio * radio;
     }
 }
